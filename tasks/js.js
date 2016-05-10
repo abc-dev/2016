6deb96eee15f00d6bd48b3647ebd3c2e920f0da5
Darlan Mendonça <darlanmendonca@gmail.com>
2016-05-10 13:46:06 -0300

    separate gulp tasks by file, and adjust .editorconfig

---
+++ tasks/js.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let gulp = require('gulp');
+let plumber = require('gulp-plumber');
+let uglify = require('gulp-uglify');
+let notify = require('gulp-notify');
+
+gulp.task('js', jsTask);
+
+function jsTask() {
+  return gulp
+  	.src(['src/js/**/*.js'])
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(gulp.dest('build/js'))
+		.pipe(notify('JS nice!'));
+};
